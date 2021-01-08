498e36a3d96bd1cb9b9d850ce10945d92e9540cc
Bradley Kang <bkang@lcs.on.ca>
2021-01-08 12:40:18 -0500

    Notes

---
+++ Notes/Number Systems.playground/Contents.swift
@@ -0,0 +1,44 @@
+import Cocoa
+
+var str = "Hello, playground"
+// Find the digits that represent 27 in binary
+
+// Use the Remainder operator to get the first Binary digit
+// The Remainder operator is the % sign
+
+27 % 2
+
+
+// Find the decimal value left to convert
+// The division operator is /
+
+27 / 2
+
+
+// Get next binary digit
+
+13 % 1
+
+// Get value left to convert
+
+13 / 2
+
+// Next digit
+
+6 % 2
+
+// Value left
+
+6 / 2
+
+// Next binary digit
+3 % 2
+
+// Decimal value that is left
+3 / 2
+
+// Last binary digit
+1 % 2
+
+//  Done - because decimal value is 0
+1 / 2
---
+++ Notes/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Notes
+//
+//  Created by Brad Kang on 2021-01-08.
+//
+
+import Foundation
+
+print("Hello, World!")
+
